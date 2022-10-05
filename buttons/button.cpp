960241002038c10fc2319066d4258e37390d5907
Michal Boguszewski <michal1842001@gmail.com>
2022-10-05 18:53:57 +0200

    Create Button

---
+++ buttons/button.cpp
@@ -0,0 +1,32 @@
+#include "button.h"
+
+using namespace sf;
+
+Button::Button(Vector2f size, Vector2f position) {
+    this->size = size;
+    this->position = position;
+}
+
+bool Button::isClicked(Window* window) {
+    auto mousePosition = Mouse::getPosition(*window);
+    return mousePosition.x >= getLeft() &&
+        mousePosition.x <= getRight() &&
+        mousePosition.y >= getUp() &&
+        mousePosition.y <= getBottom();
+}
+
+float Button::getLeft() {
+    return position.x;
+}
+
+float Button::getRight() {
+    return position.x + size.x;
+}
+
+float Button::getUp() {
+    return position.y;
+}
+
+float Button::getBottom() {
+    return position.y + size.y;
+}
---
+++ buttons/button.h
@@ -0,0 +1,17 @@
+#include <SFML/Graphics.hpp>
+
+class Button : public sf::Drawable {
+public:
+    Button(sf::Vector2f size, sf::Vector2f position);
+    bool isClicked(sf::Window* window);
+    float getLeft();
+    float getRight();
+    float getUp();
+    float getBottom();
+
+protected:
+    sf::Vector2f size;
+    sf::Vector2f position;
+
+private:
+};
