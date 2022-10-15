34b2f7f9a2e92b8b758cd40f3fe98868da8682dc
Michal Boguszewski <michal1842001@gmail.com>
2022-10-15 09:28:14 +0200

    Fix TouchPointData

--- touchDetectors/touchedPointData.h
+++ touchDetectors/touchedPointData.h
@@ -4,7 +4,7 @@
 
 struct TouchedPointData {
     TouchedPointData(int polygonIndex, int pointIndex, Polygon polygon) 
-    : polygonIndex(pointIndex), pointIndex(pointIndex), polygon(polygon) {}
+    : polygonIndex(polygonIndex), pointIndex(pointIndex), polygon(polygon) {}
 
     int polygonIndex;
     int pointIndex;
